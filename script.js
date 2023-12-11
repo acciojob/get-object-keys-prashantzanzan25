f36c4a688ec98126c733f911f6e41297f5966dd9
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-12-11 12:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var student = {
+	name: 'John Doe'
+};
+
+Object.prototype.getKeys = function() {
+	return Object.keys(this);
+};
+
+var keysArray = student.getKeys();
+
+console.log(keysArray);
